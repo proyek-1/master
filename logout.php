4d1d9606f2ae8483c023a031d3af5a574dbd0ec1
episkuswono <episkuswono@gmail.com>
2019-04-08 20:41:57 +0700

    logout untuk keluar dari web

--- logout.php
+++ logout.php
@@ -2,6 +2,7 @@
    session_start();
    session_destroy();
 ?>
+
 <div align="center">
   <h2>Anda telah berhasil logout..</h2>
   Silahkan klik <a href="homeKop.html">disini</a> untuk login kembali
