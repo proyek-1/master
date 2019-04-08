e7732e473dbd1684b5e1d42dce5e5995d8d86a30
episkuswono <episkuswono@gmail.com>
2019-04-08 20:33:49 +0700

    home mengganti nama, logo home menjadi logout dan mengganti link

--- konek.php
+++ konek.php
@@ -1,24 +1,11 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php  
-            $mysqli = mysqli_connect('localhost', 'root', '', 'koperasi');
-	
-            if($mysqli){
-                header('sukses');
-            }else{
-                header('gagal');
-            }
-        ?>
-        <br>
-    </body>
-</html>
+<?php  
+
+$server = "localhost";
+$user = "root";
+$password = "";
+$nama_database = "koperasi";
+
+$connect = mysqli_connect($server, $user, $password, $nama_database);
+?>
+
+
