8c57f3c7a274c41242193c84f4ae43f0a0bebbcb
episkuswono <episkuswono@gmail.com>
2019-04-12 07:36:21 +0700

    hemm bingung ckckck

--- login2.php
+++ login2.php
@@ -4,7 +4,7 @@ include 'konek.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-if (!empty($username) && !empty($password)) {
+if (!empty($username) && !empty($password))  {
    $sql = mysqli_query($connect, "SELECT * FROM pegawai WHERE username = '$username' AND password = '$password'");
    $result = mysqli_num_rows($sql);
    if ($result) {
