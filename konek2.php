42f9759a4227365d7bb3a6d94ff9be0c5a81a6d2
episkuswono <episkuswono@gmail.com>
2019-04-16 13:19:06 +0700

    test

--- konek2.php
+++ konek2.php
@@ -4,4 +4,10 @@ $user = "root";
 $password = "";
 $nama_database = "koperasi";
 $connect = mysqli_connect($server, $user, $password, $nama_database);
+
+if($connect){
+    echo "";
+}else{
+    die("koneksi gagal karena".mysqli_connect_error());
+}
 ?>
