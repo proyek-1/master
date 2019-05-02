0bb2d0a748a145a0af4c56023485360863cc1f8f
episkuswono <episkuswono@gmail.com>
2019-05-02 19:11:12 +0700

    menggunakan inden line untuk mengatur kerapian text

--- dataangsuran.php
+++ dataangsuran.php
@@ -1,191 +1,191 @@
-<?php
-error_reporting (E_ALL ^ E_WARNING || E_NOTICE);
-	session_start();
-	ob_start();
-	
+	<?php
+	error_reporting (E_ALL ^ E_WARNING || E_NOTICE);
+		session_start();
+		ob_start();
+		
 
-	// echo "Welcome" .$_SESSION['username'];
-?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Infinity | By Distinctive Themes">
-    <meta name="author" content="Distinctive Themes">
-    <link rel="shortcut icon" href="assets/img/favicon.ico">
+		// echo "Welcome" .$_SESSION['username'];
+	?>
+	<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<meta name="description" content="Infinity | By Distinctive Themes">
+			<meta name="author" content="Distinctive Themes">
+			<link rel="shortcut icon" href="assets/img/favicon.ico">
 
-    <title>Koperasi Maju Jaya</title>
+			<title>Koperasi Maju Jaya</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
+			<!-- Bootstrap core CSS -->
+			<link href="assets/css/bootstrap.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->   
-    <link href="assets/css/elusive-webfont.css" rel="stylesheet" >
-    <link href="assets/css/animate.css" rel="stylesheet">
-    <link href="assets/css/pe-icons.css" rel="stylesheet">
- 	<link href="assets/css/style.css" rel="stylesheet">
-    
-    <link href='http://fonts.googleapis.com/css?family=Raleway:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
-    
-    <script src="assets/js/jquery.min.js"></script>
-  <script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
-  
-  </head>
+			<!-- Custom styles for this template -->   
+			<link href="assets/css/elusive-webfont.css" rel="stylesheet" >
+			<link href="assets/css/animate.css" rel="stylesheet">
+			<link href="assets/css/pe-icons.css" rel="stylesheet">
+		<link href="assets/css/style.css" rel="stylesheet">
+			
+			<link href='http://fonts.googleapis.com/css?family=Raleway:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+			
+			<script src="assets/js/jquery.min.js"></script>
+		<script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
+		
+		</head>
 
-<body data-spy="scroll" data-offset="0" data-target="#navbar-main">
+	<body data-spy="scroll" data-offset="0" data-target="#navbar-main">
 
-  <div id="preloader"></div>
+		<div id="preloader"></div>
 
-  	<div id="search-wrapper">
-  		<input id="search-box" placeholder="Search" />
-  	</div>  
-  
-  	<div id="navbar-main">
-		<div class="navbar navbar-default navbar-fixed-top">
-			<div class="container">
-				<div class="navbar-header">				
-					<a class="navbar-brand" href="index.php"><h1>Koperasi Maju Jaya</h1></a>
-					<button type="button" class="navbar-toggle dropdown-toggle" id="open-menu" data-toggle="dropdown" data-target="#main-navigation">
-						<i class="el-icon-lines"></i>
-					</button>
-				</div>
-				<div class="navbar-collapse collapse" id="main-navigation">
-					<ul class="nav navbar-nav">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="register.php">Daftar</a></li>
-						
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Transaksi <b class="caret"></b></a>
-							<ul class="dropdown-menu">
-							<li><a href="peminjaman2.php">Peminjaman</a></li>
-								<li><a href="angsuran.php">Angsuran</a></li>
-
-								<li><a href="dataangsuran.php">Data Angsuran</a></li>
-								<li><a href="DataPeminjaman2.php">Data Peminjaman</a></li>
-							</ul>
-						</li>
-							<li><a href="kontak.php">Kontak</a></li>
+			<div id="search-wrapper">
+				<input id="search-box" placeholder="Search" />
+			</div>  
+		
+			<div id="navbar-main">
+			<div class="navbar navbar-default navbar-fixed-top">
+				<div class="container">
+					<div class="navbar-header">				
+						<a class="navbar-brand" href="index.php"><h1>Koperasi Maju Jaya</h1></a>
+						<button type="button" class="navbar-toggle dropdown-toggle" id="open-menu" data-toggle="dropdown" data-target="#main-navigation">
+							<i class="el-icon-lines"></i>
+						</button>
+					</div>
+					<div class="navbar-collapse collapse" id="main-navigation">
+						<ul class="nav navbar-nav">
+									<li><a href="index.php">Home</a></li>
+									<li><a href="register.php">Daftar</a></li>
 							
 							<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Login<b class="caret"></b></a>
-						<ul class="dropdown-menu">
-							<li><a href="login2.php">Admin</a></li>
-							<li><a href="login3.php">Anggota</a></li>
-						</ul>
-					</li>
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Transaksi <b class="caret"></b></a>
+								<ul class="dropdown-menu">
+								<li><a href="peminjaman2.php">Peminjaman</a></li>
+									<li><a href="angsuran.php">Angsuran</a></li>
 
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?=$_SESSION['username']?></a>
-							<div class="dropdown-menu">
-								<h5><a href="logout.php" style="color: black">&nbsp; &nbsp; LOGOUT</a></h5>
-							</div>
+									<li><a href="dataangsuran.php">Data Angsuran</a></li>
+									<li><a href="DataPeminjaman2.php">Data Peminjaman</a></li>
+								</ul>
+							</li>
+								<li><a href="kontak.php">Kontak</a></li>
+								
+								<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Login<b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><a href="login2.php">Admin</a></li>
+								<li><a href="login3.php">Anggota</a></li>
+							</ul>
 						</li>
 
-						<li> <a href="#" class="search-trigger"><i class="el-icon-search"></i></a></li>
-					</ul>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?=$_SESSION['username']?></a>
+								<div class="dropdown-menu">
+									<h5><a href="logout.php" style="color: black">&nbsp; &nbsp; LOGOUT</a></h5>
+								</div>
+							</li>
+
+							<li> <a href="#" class="search-trigger"><i class="el-icon-search"></i></a></li>
+						</ul>
+					</div>
 				</div>
 			</div>
-		</div>
-    </div>
+			</div>
 
-	<section class="textdivider single-post-header">
-		<div class="container">
-			<h1 class="fade-down">Data Angsuran</h1>
-			<hr class="mb60">
-		</div>
-	</section>
-
-
-	<!--  CONTENT WRAPPER -->
-	<div id="content-wrapper">		
-		
-		<section class="container page-section">
-			<div class="row white">
-				<div class="fade-up col-md-offset-2 col-md-8 single-post-content">
-				<p><a href="angsuran.php" style="color: black;">Tambah Data</a><br><br><p>
-  <table class="table table-bordered table-striped" border="1" align="center" cellpadding="5" cellspacing="2">
-    <thead>
-      <tr>
-        <th>ID Angsuran</th>
-        <th>Tanggal Angsuran</th>
-        <th>ID Anggota</th>
-        <th>Nama</th>
-        <th>Sisa Pinjaman</th>
-        <th>Angsuran Bulanan</th>
-        <th>Denda</th>
-        <th>Total Bayar</th>
-        <th>Bayar Angsuran</th>
-      </tr>
-    </thead>
-    
-    <tbody>
-      <?php
-        require_once 'konek.php';
-        $query = mysqli_query($mysqli, "SELECT * FROM angsuran");
-        while($hasil  = mysqli_fetch_assoc($query)) {
-      ?>
-		
-      <tr>
-        <td><?php echo $hasil['id_angsuran']?></td>
-        <td><?php echo $hasil['tanggal_angsuran']?></td>
-        <td><?php echo $hasil['id_anggota']?></td>
-        <td><?php echo $hasil['nama']?></td>
-        <td><?php echo $hasil['sisa_pinjaman']?></td>
-        <td><?php echo $hasil['angsuran_bulanan']?></td>
-        <td><?php echo $hasil['denda']?></td>
-        <td><?php echo $hasil['total_bayar']?></td>
-        <td><?php echo $hasil['bayar_angsuran']?></td>
-        <td><center><a href="from_edit.php?id=<?php echo $hasil['id'];?>">Edit</a> &nbsp
-              <a href="proses_hapus.php?id=<?php echo $hasil['id'];?>">hapus</a>
-          </center>
-          </td>
-      </tr>
-      <?php }?>
-    </tbody>
-	  </div>
-	  </section>
-  </table>
-</section>
-        </div>
-      </div>
-    </div>
-  </header>
-			</div><!-- row -->
-		</section>
-				
-		<section class="section-divider textdivider divider6 page-section">
+		<section class="textdivider single-post-header">
 			<div class="container">
-				<h1 class="fade-down">Jl Soekarno Hatta no 62, Kota Malang</h1>
-				<hr>
-				<div class="fade-up">
-					<p>kopmj@gmail.com</p>
-					<p>+62 856 876 987</p>
-				</div>
+				<h1 class="fade-down">Data Angsuran</h1>
+				<hr class="mb60">
 			</div>
 		</section>
-	
-	<!-- CONTENT WRAPPER -->
 
-	<div id="footerwrap">
-		<div class="container">
-			<div class="btt-wrapper fade-down"><a class="btt-link smoothScroll" href="#headerwrap"><i class="el-icon-chevron-up"></i></a></div>
-			<ul id="footer-social" class="fade-down">
-				<li><a href="#" target="_blank" title="Like Us"><i class="el-icon-twitter"></i></a></li>
-				<li><a href="#" target="_blank" title="Like Us"><i class="el-icon-facebook"></i></a></li>
-				<li><a href="#" target="_blank" title="Like Us"><i class="el-icon-googleplus"></i></a></li>
-				<li><a href="#" target="_blank" title="Like Us"><i class="el-icon-flickr"></i></a></li>
-				<li><a href="#" target="_blank" title="Like Us"><i class="el-icon-youtube"></i></a></li>
-				<li><a href="#" target="_blank" title="Like Us"><i class="el-icon-rss"></i></a></li>
-			</ul>
-			<h4 class="fade-up">Koperasi Maju Jaya<a href=</a> - Copyright 2019</h4>
+
+		<!--  CONTENT WRAPPER -->
+		<div id="content-wrapper">		
+			
+			<section class="container page-section">
+				<div class="row white">
+					<div class="fade-up col-md-offset-2 col-md-8 single-post-content">
+					<p><a href="angsuran.php" style="color: black;">Tambah Data</a><br><br><p>
+		<table class="table table-bordered table-striped" border="1" align="center" cellpadding="5" cellspacing="2">
+			<thead>
+				<tr>
+					<th>ID Angsuran</th>
+					<th>Tanggal Angsuran</th>
+					<th>ID Anggota</th>
+					<th>Nama</th>
+					<th>Sisa Pinjaman</th>
+					<th>Angsuran Bulanan</th>
+					<th>Denda</th>
+					<th>Total Bayar</th>
+					<th>Bayar Angsuran</th>
+				</tr>
+			</thead>
+			
+			<tbody>
+				<?php
+					require_once 'konek.php';
+					$query = mysqli_query($mysqli, "SELECT * FROM angsuran");
+					while($hasil  = mysqli_fetch_assoc($query)) {
+				?>
+			
+				<tr>
+					<td><?php echo $hasil['id_angsuran']?></td>
+					<td><?php echo $hasil['tanggal_angsuran']?></td>
+					<td><?php echo $hasil['id_anggota']?></td>
+					<td><?php echo $hasil['nama']?></td>
+					<td><?php echo $hasil['sisa_pinjaman']?></td>
+					<td><?php echo $hasil['angsuran_bulanan']?></td>
+					<td><?php echo $hasil['denda']?></td>
+					<td><?php echo $hasil['total_bayar']?></td>
+					<td><?php echo $hasil['bayar_angsuran']?></td>
+					<td><center><a href="from_edit.php?id=<?php echo $hasil['id'];?>">Edit</a> &nbsp
+								<a href="proses_hapus.php?id=<?php echo $hasil['id'];?>">hapus</a>
+						</center>
+						</td>
+				</tr>
+				<?php }?>
+			</tbody>
+			</div>
+			</section>
+		</table>
+	</section>
+					</div>
+				</div>
+			</div>
+		</header>
+				</div><!-- row -->
+			</section>
+					
+			<section class="section-divider textdivider divider6 page-section">
+				<div class="container">
+					<h1 class="fade-down">Jl Soekarno Hatta no 62, Kota Malang</h1>
+					<hr>
+					<div class="fade-up">
+						<p>kopmj@gmail.com</p>
+						<p>+62 856 876 987</p>
+					</div>
+				</div>
+			</section>
+		
+		<!-- CONTENT WRAPPER -->
+
+		<div id="footerwrap">
+			<div class="container">
+				<div class="btt-wrapper fade-down"><a class="btt-link smoothScroll" href="#headerwrap"><i class="el-icon-chevron-up"></i></a></div>
+				<ul id="footer-social" class="fade-down">
+					<li><a href="#" target="_blank" title="Like Us"><i class="el-icon-twitter"></i></a></li>
+					<li><a href="#" target="_blank" title="Like Us"><i class="el-icon-facebook"></i></a></li>
+					<li><a href="#" target="_blank" title="Like Us"><i class="el-icon-googleplus"></i></a></li>
+					<li><a href="#" target="_blank" title="Like Us"><i class="el-icon-flickr"></i></a></li>
+					<li><a href="#" target="_blank" title="Like Us"><i class="el-icon-youtube"></i></a></li>
+					<li><a href="#" target="_blank" title="Like Us"><i class="el-icon-rss"></i></a></li>
+				</ul>
+				<h4 class="fade-up">Koperasi Maju Jaya<a href=</a> - Copyright 2019</h4>
+			</div>
 		</div>
-	</div>
 
-	<script type="text/javascript" src="assets/js/bootstrap.js"></script>
-    <script type="text/javascript" src="assets/js/plugins.js"></script>
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCWDPCiH080dNCTYC-uprmLOn2mt2BMSUk&amp;sensor=true"></script>
-	<script type="text/javascript" src="assets/js/init.js"></script>
+		<script type="text/javascript" src="assets/js/bootstrap.js"></script>
+			<script type="text/javascript" src="assets/js/plugins.js"></script>
+			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCWDPCiH080dNCTYC-uprmLOn2mt2BMSUk&amp;sensor=true"></script>
+		<script type="text/javascript" src="assets/js/init.js"></script>
 
-  </body>
-</html>
+		</body>
+	</html>
--- register.php
+++ register.php
@@ -1,151 +1,151 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Infinity | By Distinctive Themes">
-    <meta name="author" content="Distinctive Themes">
-    <link rel="shortcut icon" href="assets/img/favicon.ico">
+	<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<meta name="description" content="Infinity | By Distinctive Themes">
+			<meta name="author" content="Distinctive Themes">
+			<link rel="shortcut icon" href="assets/img/favicon.ico">
 
-    <title>Koperasi Maju Jaya</title>
+			<title>Koperasi Maju Jaya</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
+			<!-- Bootstrap core CSS -->
+			<link href="assets/css/bootstrap.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->   
-    <link href="assets/css/elusive-webfont.css" rel="stylesheet" >
-    <link href="assets/css/animate.css" rel="stylesheet">
-    <link href="assets/css/pe-icons.css" rel="stylesheet">
- 	<link href="assets/css/style.css" rel="stylesheet">
-    
-    <link href='http://fonts.googleapis.com/css?family=Raleway:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
-    
-    <script src="assets/js/jquery.min.js"></script>
-	<script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
-    
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="assets/js/html5shiv.js"></script>
-      <script src="assets/js/respond.min.js"></script>
-    <![endif]-->
-  </head>
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-<!--===============================================================================================-->
-	
-<body data-spy="scroll" data-offset="0" data-target="#navbar-main">
-
-  <div id="preloader"></div>
-<div id="navbar-main"> </div>
-	<div class="" align="center">
-	
-	<div class="wrap-login100 p-l-85 p-r-85 p-t-55 p-b-55">
-				<form action="proses-input.php" method="POST" class="login100-form validate-form flex-sb flex-w">
-					<span class="login100-form-title p-b-32">
-						<p class="tulisan_login"><h3 align="center">Register</h3></p>
-					</span>
-					<span class="txt1 p-b-11">
-					Nama Anggota:
-					</span>
-					<div class="wrap-input100 validate-input m-b-36" data-validate = "Username is required">
-						<input class="input100" type="text" name="nama" placeholder="Nama">
-						<span class="focus-input100"></span>
-					</div>
-
-					<span class="txt1 p-b-11">
-					<br>Password:
-					</span>
-					<div class="wrap-input100 validate-input m-b-36" data-validate = "Password is required">
-						<input class="input100" type="password" name="password" placeholder="Password">
-						<span class="focus-input100"></span>
-          </div>
-
-					<span class="txt1 p-b-11">
-					<br>Jenis Kelamin:
-					</span>
-					<div class="wrap-input100 validate-input m-b-36" data-validate = "Alamat is required">
-					<input type="radio" name="jenis_kelamin" id="L" value="L">
-				  <label for="L">Laki-Laki</label><br>
-				  <input type="radio" name="jenis_kelamin" id="P" value="P">
-				  <label for="P">Perempuan</label><br>
-          </div>
-
-          <span class="txt1 p-b-11">
-					<br>Alamat:
-					</span>
-					<div class="wrap-input100 validate-input m-b-36" data-validate = "Alamat is required">
-						<input class="input100" type="text" name="alamat" placeholder="Alamat">
-						<span class="focus-input100"></span>
-          </div>
-
-					<span class="txt1 p-b-11">
-					<br>Pekerjaan:
-					</span>
-					<div class="wrap-input100 validate-input m-b-36" data-validate = "Pekerjaan is required">
-						<input class="input100" type="text" name="pekerjaan" placeholder="Perkejaan">
-						<span class="focus-input100"></span>
-          </div>
-
-					<span class="txt1 p-b-11">
-					<br>No Telepon:
-					</span>
-					<div class="wrap-input100 validate-input m-b-36" data-validate = "No Telepon is required">
-						<input class="input100" type="text" name="no_handphone" placeholder="No Telepon">
-						<span class="focus-input100"></span>
-					</div>
+			<!-- Custom styles for this template -->   
+			<link href="assets/css/elusive-webfont.css" rel="stylesheet" >
+			<link href="assets/css/animate.css" rel="stylesheet">
+			<link href="assets/css/pe-icons.css" rel="stylesheet">
+		<link href="assets/css/style.css" rel="stylesheet">
+			
+			<link href='http://fonts.googleapis.com/css?family=Raleway:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+			
+			<script src="assets/js/jquery.min.js"></script>
+		<script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
+			
+			<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+			<!--[if lt IE 9]>
+				<script src="assets/js/html5shiv.js"></script>
+				<script src="assets/js/respond.min.js"></script>
+			<![endif]-->
+		</head>
+	<!--===============================================================================================-->	
+		<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+	<!--===============================================================================================-->	
+		<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+	<!--===============================================================================================-->	
+		<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+	<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="css/util.css">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+	<!--===============================================================================================-->
 		
-					
-					
-					<div class="container-login100-form-btn">
-					  <button type="submit" name="tambah" value="Tambah" class="login100-form-btn"> 
-							Register
-						</button>
+	<body data-spy="scroll" data-offset="0" data-target="#navbar-main">
 
-						&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-
-						<button> 
-							<a href="index.php" style="color: black">Kembali</a>
-						</button>
+		<div id="preloader"></div>
+	<div id="navbar-main"> </div>
+		<div class="" align="center">
+		
+		<div class="wrap-login100 p-l-85 p-r-85 p-t-55 p-b-55">
+					<form action="proses-input.php" method="POST" class="login100-form validate-form flex-sb flex-w">
+						<span class="login100-form-title p-b-32">
+							<p class="tulisan_login"><h3 align="center">Register</h3></p>
+						</span>
+						<span class="txt1 p-b-11">
+						Nama Anggota:
+						</span>
+						<div class="wrap-input100 validate-input m-b-36" data-validate = "Username is required">
+							<input class="input100" type="text" name="nama" placeholder="Nama">
+							<span class="focus-input100"></span>
 						</div>
-					</div>
 
-					</div>
+						<span class="txt1 p-b-11">
+						<br>Password:
+						</span>
+						<div class="wrap-input100 validate-input m-b-36" data-validate = "Password is required">
+							<input class="input100" type="password" name="password" placeholder="Password">
+							<span class="focus-input100"></span>
+						</div>
 
-				</form>
-			</div>
-	<!--  CONTENT WRAPPER -->
-	<div id="content-wrapper">
-<!-- row -->		
-</div>
+						<span class="txt1 p-b-11">
+						<br>Jenis Kelamin:
+						</span>
+						<div class="wrap-input100 validate-input m-b-36" data-validate = "Alamat is required">
+						<input type="radio" name="jenis_kelamin" id="L" value="L">
+						<label for="L">Laki-Laki</label><br>
+						<input type="radio" name="jenis_kelamin" id="P" value="P">
+						<label for="P">Perempuan</label><br>
+						</div>
 
+						<span class="txt1 p-b-11">
+						<br>Alamat:
+						</span>
+						<div class="wrap-input100 validate-input m-b-36" data-validate = "Alamat is required">
+							<input class="input100" type="text" name="alamat" placeholder="Alamat">
+							<span class="focus-input100"></span>
+						</div>
+
+						<span class="txt1 p-b-11">
+						<br>Pekerjaan:
+						</span>
+						<div class="wrap-input100 validate-input m-b-36" data-validate = "Pekerjaan is required">
+							<input class="input100" type="text" name="pekerjaan" placeholder="Perkejaan">
+							<span class="focus-input100"></span>
+						</div>
+
+						<span class="txt1 p-b-11">
+						<br>No Telepon:
+						</span>
+						<div class="wrap-input100 validate-input m-b-36" data-validate = "No Telepon is required">
+							<input class="input100" type="text" name="no_handphone" placeholder="No Telepon">
+							<span class="focus-input100"></span>
+						</div>
+			
+						
+						
+						<div class="container-login100-form-btn">
+							<button type="submit" name="tambah" value="Tambah" class="login100-form-btn"> 
+								Register
+							</button>
+
+							&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+
+							<button> 
+								<a href="index.php" style="color: black">Kembali</a>
+							</button>
+							</div>
+						</div>
+
+						</div>
+
+					</form>
+				</div>
+		<!--  CONTENT WRAPPER -->
+		<div id="content-wrapper">
+	<!-- row -->		
 	</div>
-</div>
-<!-- CONTENT WRAPPER -->
-<script type="text/javascript" src="assets/js/bootstrap.js"></script>
-<script type="text/javascript" src="assets/js/plugins.js"></script>
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCWDPCiH080dNCTYC-uprmLOn2mt2BMSUk&amp;sensor=true"></script>
-<script type="text/javascript" src="assets/js/init.js"></script>
-	
-	
 
-</body>
-</html>
+		</div>
+	</div>
+	<!-- CONTENT WRAPPER -->
+	<script type="text/javascript" src="assets/js/bootstrap.js"></script>
+	<script type="text/javascript" src="assets/js/plugins.js"></script>
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCWDPCiH080dNCTYC-uprmLOn2mt2BMSUk&amp;sensor=true"></script>
+	<script type="text/javascript" src="assets/js/init.js"></script>
+		
+		
+
+	</body>
+	</html>
