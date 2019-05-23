28ef03f7afa76ff9bf7433f733a6dcf61544f245
episkuswono <episkuswono@gmail.com>
2019-05-23 10:21:16 +0700

    ini

--- proses_hapus.php
+++ proses_hapus.php
@@ -12,8 +12,13 @@ $sql = "DELETE FROM angsuran WHERE id_angsuran = '$_GET[id_angsuran]'";
     } -->
 
     <?php 
+    // error_reporting (E_ALL ^ E_WARNING || E_NOTICE);
+    include ("konek.php");
 $mysqli = mysqli_connect('localhost', 'root', '', 'koperasi');
-mysqli_query($mysqli, "DELETE FROM angsuran WHERE id_angsuran = '$_GET[id_angsuran]'");
+$id = $_GET['id_angsuran'];
+mysqli_query($mysqli, "DELETE FROM angsuran WHERE id_angsuran = '$id'")or die(mysql_error());
 echo "<script type='text/javascript'>alert('Data Terhapus!');</script>";
-// echo "<script type='text/javascript'>location='index.php';</script>";
+echo "<script type='text/javascript'>location='dataangsuran.php';</script>";
 ?> 
+
+<!-- DELETE FROM angsuran WHERE angsuran.id_angsuran = '$_GET[id_angsuran]' -->
